3a63bd7b8d3c20bd3d5078c05aa5d578ec559f63
HerrGalgen <kreuzer.fabian4@gmail.com>
2022-07-21 13:20:14 +0200

    remove unnecessary code

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,6 @@ String          command[9] = {};
 int             pos= 0;
 int             actualHeight = 0;
 
-void moveDesk(char dir);
 void printArray();
 char *strToChar(String str);
 void setHeight();
@@ -74,7 +73,7 @@ void loop() {
 
         //Command finished:
         if(hexVal == "7e" || pos > 8 ) {
-
+            
             pos = 0;
             setHeight();
             printArray();
@@ -94,55 +93,6 @@ void moveDeskToHeight(int height) {
             
     }
 
-}
-
-void moveDesk(char dir) {
-    Serial.print("Move desk: ");
-    // Move desk
-
-    switch(dir) {
-        case 'u':
-            Serial.println("Up");
-
-            digitalWrite(HS1, HIGH);
-            delay(2000);
-            digitalWrite(HS1, LOW);
-
-            break;
-        case 'd':
-            Serial.println("Down");
-            
-            digitalWrite(HS0, HIGH);
-            delay(2000);
-            digitalWrite(HS0, LOW);
-
-            break;
-        case '1':
-            Serial.println("1");
-            digitalWrite(HS0, HIGH);
-            digitalWrite(HS1, HIGH);
-            delay(500);
-            digitalWrite(HS0, LOW);
-            digitalWrite(HS1, LOW);
-            break;
-        case '2':
-            Serial.println("2");
-            break;
-        case '3':
-            Serial.println("3");
-            break;
-        case '4':
-            Serial.println("4");
-            break;
-        case 'm':
-            Serial.println("memory");
-            break;
-        default:
-            Serial.println("Unknown");
-            break;
-    }
-
-    delay(1000);
 }
 
  char *strToChar(String str) {
